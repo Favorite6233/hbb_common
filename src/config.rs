d1d26fa2673a91546ec2c21b1b76f4f5121ec2a9
Favorite6233 <github@favorite6233.net>
2026-06-20 14:42:26 +0300

    Set custom rendezvous server

--- src/config.rs
+++ src/config.rs
@@ -117,8 +117,11 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+// pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
+// pub const RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["rd.favorite6233.net"];
+pub const RS_PUB_KEY: &str = "IDfviFYSYnHncspJGKTrSHTgbgt4BwlRaWfzgmaWSMQ=";
+
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
